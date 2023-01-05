88acde1c2bba3953b8ff74849d9a01c01cb8b9c9
Kyle <kyle.fields@gmail.com>
2023-01-05 18:50:44 -0500

    Adding the election audit code.

---
+++ HelloVariableWorld.py
@@ -0,0 +1,10 @@
+name = "Kyle"
+country = "United States"
+age = 43
+hourly_wage = 25
+satisfied = False
+daily_wage = hourly_wage * 8
+print(name + " lives in the " + country)
+print(name + " is " + str(age) + " years old.")
+print(name + " makes $" + str(daily_wage) + " per day.")
+print("Are you satisfied with your current wage?" + "" + str(satisfied))
---
+++ python_loops.py
@@ -0,0 +1,8 @@
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+for num in range(5):
+    print(num)
+
